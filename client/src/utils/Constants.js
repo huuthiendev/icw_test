6cd8b360333f63556ea5fe9b7ef138501b53be6e
huuthiendev <huuthiendev@gmail.com>
2022-02-14 02:00:00 +0700

    update constants

---
+++ client/src/utils/Constants.js
@@ -0,0 +1,12 @@
+const constants = {
+  API_ENDPOINT: "http://localhost:1337",
+  API_JOIN_ROOM: "/socket-api/join-room",
+  API_LEAVE_ROOM: "/socket-api/leave-room",
+  API_SEND_MESSAGE: "/socket-api/send-message",
+  API_CHECK_USER_IN_ROOM: "/api/check-user-in-room",
+
+  STORAGE_KEY: "USER_INFO",
+  EVENT_INCOMING_MESSAGE: "incoming_message"
+};
+
+export default constants;
